6d2173f7b4705ac68ff7043a8190a45e3db6b866
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-05-02 00:16:46 +0300

    START APP

---
+++ android/app/src/main/kotlin/com/example/mahmoud_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mahmoud_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mahmoud_store/mahmoud_store.dart';
+
+void main() {
+  runApp(const MahmoudStore());
+}
