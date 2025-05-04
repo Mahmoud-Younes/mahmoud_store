40286113ff22a2839ac0a7d03f5061d7353cb3cb
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-05-05 00:33:56 +0300

    setup  firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mahmoud_store/firebase_options.dart';
 import 'package:mahmoud_store/mahmoud_store.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MahmoudStore());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const MahmoudStore());
+  });
 }
