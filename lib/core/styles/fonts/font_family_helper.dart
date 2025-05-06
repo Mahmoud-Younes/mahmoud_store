20b2394c00c51edf6cf88db4ca15374d1a1bb4b6
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-05-07 01:22:08 +0300

    add screenutils and font family and weight and folder lang and add base routes file   and context_extension   and  routes

--- lib/core/styles/fonts/font_family_helper.dart
+++ lib/core/styles/fonts/font_family_helper.dart
@@ -1,19 +1,17 @@
-// import 'package:asroo_store/core/service/shared_pref/pref_keys.dart';
-// import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
+class FontFamilyHelper {
+  const FontFamilyHelper._();
 
-// class FontFamilyHelper {
-//   const FontFamilyHelper._();
+  static const String cairoArabic = 'Cairo';
 
-//   static const String cairoArabic = 'Cairo';
+  static const String poppinsEnglish = 'Poppins';
+  //TODO:switch arabic and eng
 
-//   static const String poppinsEnglish = 'Poppins';
-
-//   static String geLocalozedFontFamily() {
-//     final currentLanguage = SharedPref().getString(PrefKeys.language);
-//     if (currentLanguage == 'ar') {
-//       return cairoArabic;
-//     } else {
-//       return poppinsEnglish;
-//     }
-//   }
-// }
+  // static String geLocalozedFontFamily() {
+  // final currentLanguage = SharedPref().getString(PrefKeys.language);
+  // if (currentLanguage == 'ar') {
+  //   return cairoArabic;
+  // } else {
+  //   return poppinsEnglish;
+  // }
+  // }
+}
--- lib/mahmoud_store.dart
+++ lib/mahmoud_store.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mahmoud_store/core/app/connectivity_controller.dart';
 import 'package:mahmoud_store/core/app/env_variable.dart';
 import 'package:mahmoud_store/core/common/no_network_screen.dart';
+import 'package:mahmoud_store/core/routes/app_routes.dart';
 
 class MahmoudStore extends StatelessWidget {
   const MahmoudStore({super.key});
@@ -12,23 +14,28 @@ class MahmoudStore extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, _) {
         if (value) {
-          return MaterialApp(
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
-                ),
-              );
-            },
-            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+              onGenerateRoute: AppRoutes.onGenerateRoute,
+              initialRoute: AppRoutes.login,
             ),
-            home: Scaffold(appBar: AppBar(title: const Text('mahmoud store'))),
           );
         } else {
           return MaterialApp(
