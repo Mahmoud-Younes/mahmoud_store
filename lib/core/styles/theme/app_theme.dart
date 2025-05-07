17ee68706c73aae9c5d041c907d83730edddf080
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-05-08 01:11:41 +0300

    color theme done and  localizations done

--- lib/core/styles/theme/app_theme.dart
+++ lib/core/styles/theme/app_theme.dart
@@ -1,36 +1,40 @@
-// import 'package:asroo_store/core/style/colors/colors_dark.dart';
-// import 'package:asroo_store/core/style/colors/colors_light.dart';
-// import 'package:asroo_store/core/style/fonts/font_family_helper.dart';
-// import 'package:asroo_store/core/style/theme/assets_extension.dart';
-// import 'package:asroo_store/core/style/theme/color_extension.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:mahmoud_store/core/styles/colors/colors_dark.dart';
+import 'package:mahmoud_store/core/styles/colors/colors_light.dart';
+import 'package:mahmoud_store/core/styles/theme/assets_extension.dart';
+import 'package:mahmoud_store/core/styles/theme/color_extension.dart';
 
-// ThemeData themeDark() {
-//   return ThemeData(
-//     scaffoldBackgroundColor: ColorsDark.mainColor,
-//     extensions: const <ThemeExtension<dynamic>>[MyColors.dark, MyAssets.dark],
-//     useMaterial3: true,
-//     textTheme: TextTheme(
-//       displaySmall: TextStyle(
-//         fontSize: 14,
-//         color: ColorsDark.white,
-//         fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
-//       ),
-//     ),
-//   );
-// }
+ThemeData themeDark() {
+  return ThemeData(
+    scaffoldBackgroundColor: ColorsDark.mainColor,
+    extensions: const <ThemeExtension<dynamic>>[
+      MyColors.dark,
+      MyAssets.dark,
+    ], //
 
-// ThemeData themeLight() {
-//   return ThemeData(
-//     scaffoldBackgroundColor: ColorsLight.mainColor,
-//     extensions: const <ThemeExtension<dynamic>>[MyColors.light, MyAssets.light],
-//     useMaterial3: true,
-//     textTheme: TextTheme(
-//       displaySmall: TextStyle(
-//         fontSize: 14,
-//         color: ColorsLight.black,
-//         fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
-//       ),
-//     ),
-//   );
-// }
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displaySmall: TextStyle(
+        fontSize: 14,
+        color: ColorsDark.white,
+        // fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
+      ),
+    ),
+  );
+}
+
+ThemeData themeLight() {
+  return ThemeData(
+    scaffoldBackgroundColor: ColorsLight.mainColor,
+    extensions: const <ThemeExtension<dynamic>>[MyColors.light, MyAssets.light],
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      displaySmall: TextStyle(
+        fontSize: 14,
+        color: ColorsLight.black,
+
+        // fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
+      ),
+    ),
+  );
+}
